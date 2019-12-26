d1667bc1a2e4688e4a2dc93025732ed86bd57492
= <=>
2019-12-25 20:31:21 -0500

    official first

---
+++ index.ts
@@ -0,0 +1,13 @@
+//@ts-check
+import * as dotenv from "dotenv";
+
+dotenv.config()
+
+/**
+ * @interface DIST_A
+ * @param PATH {string} path to distribution
+ */
+interface DIST_A {
+    PATH: string
+    
+}
